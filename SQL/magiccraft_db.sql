74ee53aeb5f91944befafe8a4c7ad4c7cd8410d4
Sasha Bridges <sashameriebridges@gmail.com>
2019-06-01 09:35:44 -0500

    Updating Website

---
+++ SQL/magiccraft_db.sql
@@ -0,0 +1,85 @@
+DROP DATABASE magiccraft_db;
+CREATE DATABASE magiccraft_db;
+
+USE magiccraft_db;
+
+CREATE TABLE magic_craft_items(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	skill_rank VARCHAR(6),
+	production_exp INT(15),
+	materials VARCHAR(200),
+    training_type VARCHAR(50)
+);
+
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ('Shyllien', 'N',	100,	'Shyllien Crystal × 5',	'Magic Conversion');
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ('Training Mana Bullet', 'N',	150,	'Shyllien Crystal × 2',	'Mana Bullet Crafting');
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ('Purified Rabbit Foot',	'D',	200,	'Shyllien × 1, Mutant Rabbit Foot × 1',	'Magic Material Crafting');
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ('Mana Bullet',	'C',	250,	'Shyllien × 1',	'Mana Bullet Crafting');
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
+INSERT INTO magic_craft_items (name, item_rank, skill_rank, production_exp, materials, training_type) VALUES ();
